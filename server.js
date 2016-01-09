45151a8930cdf6f21633f44553b688377cb69fc7
Peter Lindhard <peter.lindhard@gmail.com>
2016-01-09 11:11:17 +0100

    top tracks working

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ app.use(express.static('public'));
 
 var get_artist = function(args) {
     var emitter = new events.EventEmitter();
-
     unirest.get('https://api.spotify.com/v1/search')
         .qs(args)
         .end(function(response) {
@@ -39,6 +38,25 @@ var get_related = function(args) {
     return emitter;
 };
 
+var get_toptracks = function(args) {
+    debugger;
+    var emitter = new events.EventEmitter();
+    if(args && args.id){
+        unirest.get('https://api.spotify.com/v1/artists/' + args.id + '/top-tracks?country=DK')
+            //unirest.get('https://api.spotify.com/v1/artists/43ZHCT0cAZBISjO8DG9PnE/top-tracks?country=DK')
+            .end(function(response) {
+                if (response.ok) {
+
+                    emitter.emit('items', response.body.tracks, args);
+                }
+                else {
+                    emitter.emit('error', response.code);
+                }
+            });
+    }
+    return emitter;
+};
+
 app.get('/search/:name', function(req, res) {
     var search_related = null;
     var search_artist = get_artist({
@@ -52,8 +70,30 @@ app.get('/search/:name', function(req, res) {
         var search_related = get_related(item.artists.items[0]);
 
         search_related.on('items', function(items){
-            item.related = items.artists;
-            res.json(item)
+            var total = 0, complete = 0, errors = 0;
+            if(!items.artists){
+                this.emit('error', 404);
+            }else{
+                item.related = items.artists;
+                total = item.related.length;
+                item.related.map(function(related){
+                    get_toptracks(related)
+                        .on('items', function(items, current){
+                            debugger;
+                            current.tracks = items;
+                            current.errors = errors;
+                            complete++;
+                            if(total === complete + errors){
+                                res.json(item);
+                                }
+                            }
+                        )
+                        .on('error', function(error){
+                            errors++;
+                        }
+                    );
+                })
+            }
         });
 
         search_related.on('error', function(error){
